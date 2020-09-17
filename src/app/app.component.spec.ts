4a1d7c6a50f8eadcf69c25dd5cd7121b2112a6cc
DenisKoletic <denis.koletic@q-alliance.com>
2020-09-17 15:10:52 +0200

    rename

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'custom-loader'`, () => {
+  it(`should have as title 'create-lib'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('custom-loader');
+    expect(app.title).toEqual('create-lib');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('custom-loader app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('create-lib app is running!');
   });
 });
